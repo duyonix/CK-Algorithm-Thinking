973bc1d7712fa68a9c708a4d405ed20a0d76751b
nguyenkhoa <nguyenkhoa0721@gmail.com>
2022-01-20 21:40:53 +0700

    add bai 5, comment bai 19

--- Bai19/main.cpp
+++ Bai19/main.cpp
@@ -24,18 +24,18 @@ void calTLeft(int d)
     {
         if (i != n - 1)
         {
-            prev = a[i + 1] - d + 1;
-            L[i] = L[i + 1] - prev + (i - stop);
+            prev = a[i + 1] - d + 1; //Số tiền cần đào MỘT cột i+1 (Không quan tâm tới điều kiện cách nhau max 1)
+            L[i] = L[i + 1] - prev + (i - stop); //Cập nhật số tiền đào ở cột mới với stop dừng ở lần duyệt trước
         }
 
         if (stop == -1)
         {
             continue;
         }
-
-        for (int j = stop; j >= -1; j--) // Đặt -1 để biết là duyệt tới cột đầu rồi
+        //duyệt tiếp từ stop về tới đầu để biết stop tiếp theo dừng ở đâu thì hợp lệ
+        for (int j = stop; j >= -1; j--)
         {
-            if (j == -1)
+            if (j == -1) // Đặt -1 để biết là duyệt tới cột đầu rồi dừng duyệt tiếp
             {
                 stop = j;
                 break;
@@ -50,7 +50,7 @@ void calTLeft(int d)
     }
 }
 void calTRight(int d)
-{
+{ //Tương tự như Left nhưng này là Right
     for (int i = 0; i < n; i++)
     {
         R[i] = 0;
@@ -99,12 +99,12 @@ void chatNhiPhan(int l, int r)
     calTRight(mid);
 
     int minn = MAXX;
-
+    //Tính tiền đào nhỏ nhất có thể ở độ sâu d với mọi cột
     for (int i = 0; i < n; i++)
     {
         minn = min((L[i] + R[i] - (a[i] - mid + 1)), minn);
     }
-
+    //minn này tỉ lệ thuận với độ sâu => chặt nhị phân
     if (minn <= T)
     {
         result = mid;
