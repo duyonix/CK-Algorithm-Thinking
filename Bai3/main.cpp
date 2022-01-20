8b34b2d0521a196310e2e99bf8d5a6113a296892
DuyOnix <dtduy2210@gmail.com>
2022-01-20 19:48:39 +0700

    update

--- Bai3/main.cpp
+++ Bai3/main.cpp
@@ -10,36 +10,46 @@ using namespace std;
 ll n, m, k;
 
 // O(nm*log(nm))
-void sol1(){
+void sol1()
+{
     vector<ll> arr;
     ll s;
-    for(int i=1; i<=n; i++){
-        for (int j=1; j<=m; j++){
-            arr.push_back(i*i + j*j);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            arr.push_back(i * i + j * j);
         }
     }
     sort(arr.begin(), arr.end());
-    for (ll x: arr){
-        cout<< x<<" ";
+    for (ll x : arr)
+    {
+        // cout<< x<<" ";
     }
-    cout<<endl;
-    cout << arr[k-1];
+    cout << endl;
+    cout << arr[k - 1];
 }
 
-void sol2(){
+void sol2()
+{
     // dp not solve
 }
 
-void solution(){
+void solution()
+{
     cin >> n >> m >> k;
-    if(n*m<1e6){
+    if (n * m < 1e6)
+    {
         sol1();
-    }else{
+    }
+    else
+    {
         sol2();
     }
 }
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     solution();
--- Bai6/main.cpp
+++ Bai6/main.cpp
@@ -6,42 +6,53 @@
 
 using namespace std;
 
-const int MAXV=1e6+60;
-const int MAXN=1e3+30;
+const int MAXV = 1e6 + 60;
+const int MAXN = 1e3 + 30;
 int c[MAXV], d[MAXV], e[MAXV];
 int n, l, r;
 int L[MAXN], R[MAXN];
 
-int solution(){
-    int ans=0;
-    for(int i=0;i<n;i++){
-        if(e[R[i]]<l || R[i]<l || L[i]>r || e[R[i]]<L[i]){
-            ans=max(ans, R[i]-L[i]);
+int solution()
+{
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (e[R[i]] < l || R[i] < l || L[i] > r || e[R[i]] < L[i])
+        {
+            ans = max(ans, R[i] - L[i]);
         }
     }
     return ans;
 }
 
-void input(){
-    cin>>n>>l>>r;
-    for(int i=0;i<n;i++){
+void input()
+{
+    cin >> n >> l >> r;
+    for (int i = 0; i < n; i++)
+    {
         int x, y;
-        cin>>x>>y;
-        L[i]=x;
-        R[i]=y;
+        cin >> x >> y;
+        L[i] = x;
+        R[i] = y;
         c[x]++;
-        c[y+1]--;
+        c[y + 1]--;
     }
-    c[0]=d[0];
-    for(int i=1;i<MAXV;i++){
-        d[i]=d[i-1]+c[i];
+    c[0] = d[0];
+    for (int i = 1; i < MAXV; i++)
+    {
+        d[i] = d[i - 1] + c[i];
     }
-    for(int i=0;i<MAXV;i++){
-        if(d[i]==1){
-            if(i>r) e[i]=e[i-1];
-            else e[i]=i;
+    for (int i = 0; i < MAXV; i++)
+    {
+        if (d[i] == 1)
+        {
+            if (i > r)
+                e[i] = e[i - 1];
+            else
+                e[i] = i;
         }
-        else e[i]=e[i-1];
+        else
+            e[i] = e[i - 1];
     }
 }
 
@@ -49,7 +60,7 @@ int main()
 {
     freopen("input6_1.txt", "r", stdin);
     input();
-    int ans=solution();
-    cout<<ans<<"\n";
+    int ans = solution();
+    cout << ans << "\n";
     return 0;
 }
