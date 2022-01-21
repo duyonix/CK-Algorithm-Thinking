bf1bf0a155a6e42a5bbc25c83337b4726e0d52f6
DuyOnix <dtduy2210@gmail.com>
2022-01-21 10:14:44 +0700

    handle

--- Bai1/main.cpp
+++ Bai1/main.cpp
@@ -4,54 +4,63 @@
 
 #define MAX 1e3
 #define ll long long
-#define pii pair<int,int>
+#define pii pair<int, int>
 
 using namespace std;
-// O(log(max(a,b))) 
-ll gcd(ll a, ll b){
+// O(log(max(a,b)))
+ll gcd(ll a, ll b)
+{
     return b == 0 ? a : gcd(b, a % b);
 }
 
 // Naive
-void solution(){
-    ll  n, k;
+void solution()
+{
+    ll n, k;
     cin >> n >> k;
-    vector <ll> a(n);
-    for (ll i = 0; i < n; i++) {
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
         cin >> a[i];
     }
     ll gcd_max = a[0];
-    for (ll i = 0; i <= n-k; i++) {
+    for (ll i = 0; i <= n - k; i++)
+    {
         ll temp_gcd = a[i];
-        for (ll j = i+1; j < i+k ; ++j){
+        for (ll j = i + 1; j < i + k; ++j)
+        {
             temp_gcd = gcd(temp_gcd, a[j]);
-            cout << a[j] <<" ";
+            // cout << a[j] << " ";
         }
-        cout << endl;
-        if (temp_gcd > gcd_max){
+        // cout << endl;
+        if (temp_gcd > gcd_max)
+        {
             gcd_max = temp_gcd;
         }
     }
 
-    cout << gcd_max <<endl;
+    cout << gcd_max << endl;
 }
 
 // Giảm số lần gọi hàm gcd
-void solution1(){
-    ll  n, k;
+void solution1()
+{
+    ll n, k;
     cin >> n >> k;
-    vector <ll> a(n);
-    for (ll i = 0; i < n; i++) {
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
         cin >> a[i];
     }
-    // code 
+    // code
     ll gcd_max = a[0];
-    cout << gcd_max <<endl;
+    cout << gcd_max << endl;
 }
 
-int main(){
+int main()
+{
     freopen("input.inp", "r", stdin);
-    freopen("output.out", "w", stdout);
+    // freopen("output.out", "w", stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     solution();
