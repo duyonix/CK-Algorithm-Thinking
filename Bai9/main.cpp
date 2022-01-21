95887ec121a3631f5e963359dfe854a85c6967cd
DuyOnix <dtduy2210@gmail.com>
2022-01-21 07:38:12 +0700

    handle in onix

--- Bai9/main.cpp
+++ Bai9/main.cpp
@@ -6,66 +6,82 @@
 
 using namespace std;
 
-const int MAXN=1e3+30;
-const long long MOD=1e9+7;
+const int MAXN = 1e3 + 30;
+const long long MOD = 1e9 + 7;
 int erathones[1001];
 int n;
 long long f[1001][100];
 
-void setup(){
-    for(int i=0;i<=1000;i++){
-        for(int j=0;j<100;j++){
-            f[i][j]=-1;
+void setup()
+{
+    for (int i = 0; i <= 1000; i++)
+    {
+        for (int j = 0; j < 100; j++)
+        {
+            f[i][j] = -1;
         }
     }
 }
 
-long long calculateF(int a, int b){
-    if(f[a][b]!=-1) return f[a][b];
-    if(b<10){
-        f[a][b]=0;
+long long calculateF(int a, int b)
+{
+    if (f[a][b] != -1)
+        return f[a][b];
+    if (b < 10)
+    {
+        f[a][b] = 0;
         return 0;
     }
-    if(a==3){
-        long long ans=0;
-        for(int i=0;i<10;i++){
-            int p=b*10+i;
-            if(erathones[p]==1){
+    if (a == 3)
+    {
+        long long ans = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            int p = b * 10 + i;
+            if (erathones[p] == 1)
+            {
                 ans++;
             }
         }
-        f[a][b]=ans;
+        f[a][b] = ans;
         return ans;
     }
-    long long ans=0;
-    for(int i=0;i<10;i++){
-        int p=b*10+i;
-        if(erathones[p]==1){
-            ans=(ans+calculateF(a-1, p%100))%MOD;
+    long long ans = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        int p = b * 10 + i;
+        if (erathones[p] == 1)
+        {
+            ans = (ans + calculateF(a - 1, p % 100)) % MOD;
         }
     }
-    f[a][b]=ans;
+    f[a][b] = ans;
     return ans;
 }
 
-void setupErathones(){
-    erathones[0]=0;
-    erathones[1]=0;
-    for(int i=2;i<=1000;i++){
-        erathones[i]=true;
+void setupErathones()
+{
+    erathones[0] = 0;
+    erathones[1] = 0;
+    for (int i = 2; i <= 1000; i++)
+    {
+        erathones[i] = true;
     }
-    for(int i=2;i<=1000;i++){
-        if(erathones[i]==1){
-            for(int j=i*i;j<=1000;j+=i){
-                erathones[j]=0;
+    for (int i = 2; i <= 1000; i++)
+    {
+        if (erathones[i] == 1)
+        {
+            for (int j = i * i; j <= 1000; j += i)
+            {
+                erathones[j] = 0;
             }
         }
     }
 }
 
-
-void input(){
-    cin>>n;
+void input()
+{
+    cin >> n;
 }
 
 int main()
@@ -73,13 +89,15 @@ int main()
     setupErathones();
     setup();
     input();
-    long long ans=0;
-    for(int i=100;i<1000;i++){
-        if(erathones[i]==1){
-            long long tmp=calculateF(n-1, i%100);
-            ans=(ans+tmp)%MOD;
+    long long ans = 0;
+    for (int i = 100; i < 1000; i++)
+    {
+        if (erathones[i] == 1)
+        {
+            long long tmp = calculateF(n - 1, i % 100);
+            ans = (ans + tmp) % MOD;
         }
     }
-    cout<<ans<<"\n";
+    cout << ans << "\n";
     return 0;
 }
